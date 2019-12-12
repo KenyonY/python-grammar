0d453d9af40005035a2a51af885742ab90943fce
beidongjiedeguang <kunyuany@163.com>
2019-12-12 21:02:49 +0800

    add some plot

--- streamlit/wechat.py
+++ streamlit/wechat.py
@@ -1,18 +1,1 @@
-import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import os
-import re
-import time
 
-import itchat
-from itchat.content import *
-import time
-# import jieba
-itchat.auto_login(hotReload=True)
-
-name = 'guang'
-guang = itchat.search_friends(name)
-print(guang[0])
-# st.write(guang[0])
